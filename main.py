223597918fc62523ed68a8d6f02737a439b07154
ankylosaurus11 <dylanjkubic@gmail.com>
2024-05-31 21:03:28 -0400

    create for loop and dict in char func

--- main.py
+++ main.py
@@ -3,13 +3,21 @@ def word_counter(text):
     return len(text_list)
 
 def character_counter(words):
-    character_count = {}
+    letter_dictionary = {}
+    for i in words:
+        if i in letter_dictionary:
+            letter_dictionary[i] = +1
+        elif:
+            letter_dictionary[i] = 1
+    return letter_dictionary
 
 
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
     word_count = word_counter(file_contents)
+    individual_characters = character_counter(list(file_contents.lower()))
+    print(individual_characters)
     print(word_count)
 
 if __name__ == "__main__":
