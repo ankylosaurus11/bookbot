b99aed7b330bb2fea5c15b759a4653596161011c
ankylosaurus11 <dylanjkubic@gmail.com>
2024-06-01 06:13:42 -0400

    create aggregate func, start of loop work

--- main.py
+++ main.py
@@ -11,14 +11,27 @@ def character_counter(words):
             letter_dictionary[i] = 1
     return letter_dictionary
 
+def aggregate_data(data):
+    agg_list = []
+    alpha_data = {}
+    for i in data:
+        if i.isalpha():
+            alpha_data["char"] = i
+            print("isalpha")
+        else:
+            print("notalpha")
+    print(alpha_data)
+
 
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
     word_count = word_counter(file_contents)
     individual_characters = character_counter(list(file_contents.lower()))
+    aggregation = aggregate_data(individual_characters)
     print(individual_characters)
     print(word_count)
+
 
 if __name__ == "__main__":
     main()
